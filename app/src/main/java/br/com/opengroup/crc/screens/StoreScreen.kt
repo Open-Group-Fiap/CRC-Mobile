c6f181c3a950a86ff9a3a347fc6b4ace52cd5053
cauarojas <cauarojas@hotmail.com>
2024-11-13 10:19:02 -0300

    added all screen files

---
+++ app/src/main/java/br/com/opengroup/crc/screens/StoreScreen.kt
@@ -0,0 +1,17 @@
+package br.com.opengroup.crc.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun StoreScreen(navController: NavController) {
+
+}
+
+@Preview
+@Composable
+fun StoreScreenPreview() {
+    StoreScreen(rememberNavController())
+}
---
+++ app/src/main/java/br/com/opengroup/crc/screens/UserScreen.kt
@@ -0,0 +1,17 @@
+package br.com.opengroup.crc.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun UserScreen(navController: NavController) {
+
+}
+
+@Preview
+@Composable
+fun UserScreenPreview() {
+    UserScreen(rememberNavController())
+}
